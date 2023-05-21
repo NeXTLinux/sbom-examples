20d1837cd05dc9c20f44ca2c062f2163c96b34c2
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-05-21 20:57:32 +0000

    up

--- scripts/make_sboms.sh
+++ scripts/make_sboms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -x './gosbom' ]; then
-    echo "Command ./gosbom must exist and be executable"
+if [ ! -x './syft' ]; then
+    echo "Command ./syft must exist and be executable"
     exit 1
 fi
 
@@ -15,15 +15,15 @@ do
     # create a 'directory' friendly string
     ODIR_IMAGE=`echo ${image} | sed "s/\//_/g"`
 
-    # main work - try to create a result directory, try to create a gosbom native sbom, try to create spdx/cyclone format sboms.  if pre condition fails, skip processing and move on
+    # main work - try to create a result directory, try to create a syft native sbom, try to create spdx/cyclone format sboms.  if pre condition fails, skip processing and move on
     if ( mkdir -p ./results/${ODIR_IMAGE}/sbom ); then
-	if ( ./gosbom -q -o json registry:${image} > ./results/${ODIR_IMAGE}/sbom/gosbomjson.json ); then
-	    ./gosbom -q convert ./results/${ODIR_IMAGE}/sbom/gosbomjson.json -o spdx-json=./results/${ODIR_IMAGE}/sbom/spdx.json
-	    ./gosbom -q convert ./results/${ODIR_IMAGE}/sbom/gosbomjson.json -o cyclonedx-json=./results/${ODIR_IMAGE}/sbom/cyclonedx.json
+	if ( ./syft -q -o json registry:${image} > ./results/${ODIR_IMAGE}/sbom/gosbomjson.json ); then
+	    ./syft -q convert ./results/${ODIR_IMAGE}/sbom/gosbomjson.json -o spdx-json=./results/${ODIR_IMAGE}/sbom/spdx.json
+	    ./syft -q convert ./results/${ODIR_IMAGE}/sbom/gosbomjson.json -o cyclonedx-json=./results/${ODIR_IMAGE}/sbom/cyclonedx.json
 	else
 	    rm -f ./results/${ODIR_IMAGE}/sbom/gosbomjson.json ./results/${ODIR_IMAGE}/sbom/spdx.json ./results/${ODIR_IMAGE}/sbom/cyclonedx.json
 	    rmdir ./results/${ODIR_IMAGE}/sbom/ ./results/${ODIR_IMAGE}/
-	    echo "cannot generate gosbom SBOM for image ${image}, moving on"
+	    echo "cannot generate syft SBOM for image ${image}, moving on"
 	fi
     else
 	echo "cannot make result directory for image ${ODIR_IMAGE}, moving on"
